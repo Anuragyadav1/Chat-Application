3999aed42ac8e79f0e35bb58297f4b0ffbc6e9f8
Anurag Yadav <10anuragyadav2002@gmail.com>
2025-03-08 16:28:03 +0530

    useAuth updated

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "http://chat-application-6wxf.onrender.com/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "https://chat-application-6wxf.onrender.com/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
