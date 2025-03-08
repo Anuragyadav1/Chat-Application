b70cfa4ea4efba2d0908410fdf641b0417da538d
Anurag Yadav <10anuragyadav2002@gmail.com>
2025-03-08 16:20:01 +0530

    in backend, frontend deployed url updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-frontend-domain.com"],
+    origin: ["http://localhost:5173", "https://my-chat-app-ogqb.onrender.com"],
     credentials: true,
   })
 );
